2ce950f762a5864b066b21b4b23811bd8a8689f7
Triang3l <triang3l@yandex.ru>
2020-01-12 00:45:58 +0300

    [GPU] Root: Vulkan DLL loading

--- HbGPU.c
+++ HbGPU.c
@@ -4,6 +4,10 @@
 #include <Windows.h>
 #endif
 
+/*******************
+ * System API setup
+ *******************/
+
 void HbGPU_Root_Init(HbGPU_Root * const root, HbBool validateUsage, HbMem_Tag_Root * const memTagRoot) {
 	HbReport_Assert_Assume(root != NULL);
 	HbReport_Assert_Assume(memTagRoot != NULL);
@@ -17,8 +21,8 @@ void HbGPU_Root_Init(HbGPU_Root * const root, HbBool validateUsage, HbMem_Tag_Ro
 		HMODULE dxgiModule = LoadLibraryW(L"dxgi.dll");
 		if (dxgiModule != NULL) {
 			HbBool moduleLoaded = HbTrue;
-			moduleLoaded &= (*((FARPROC *) &root->d3d12API_i.createDXGIFactory2_i) = GetProcAddress(dxgiModule, "CreateDXGIFactory2")) != NULL;
-			moduleLoaded &= (*((FARPROC *) &root->d3d12API_i.dxgiGetDebugInterface1_i) = GetProcAddress(dxgiModule, "DXGIGetDebugInterface1")) != NULL;
+			moduleLoaded &= (*((FARPROC *) &root->api_i.d3d12_i.createDXGIFactory2_i) = GetProcAddress(dxgiModule, "CreateDXGIFactory2")) != NULL;
+			moduleLoaded &= (*((FARPROC *) &root->api_i.d3d12_i.dxgiGetDebugInterface1_i) = GetProcAddress(dxgiModule, "DXGIGetDebugInterface1")) != NULL;
 			if (!moduleLoaded) {
 				FreeLibrary(dxgiModule);
 				dxgiModule = NULL;
@@ -27,16 +31,16 @@ void HbGPU_Root_Init(HbGPU_Root * const root, HbBool validateUsage, HbMem_Tag_Ro
 		HMODULE d3d12Module = LoadLibraryW(L"D3D12.dll");
 		if (d3d12Module != NULL) {
 			HbBool moduleLoaded = HbTrue;
-			moduleLoaded &= (*((FARPROC *) &root->d3d12API_i.d3d12CreateDevice_i) = GetProcAddress(d3d12Module, "D3D12CreateDevice")) != NULL;
-			moduleLoaded &= (*((FARPROC *) &root->d3d12API_i.d3d12GetDebugInterface_i) = GetProcAddress(d3d12Module, "D3D12GetDebugInterface")) != NULL;
+			moduleLoaded &= (*((FARPROC *) &root->api_i.d3d12_i.d3d12CreateDevice_i) = GetProcAddress(d3d12Module, "D3D12CreateDevice")) != NULL;
+			moduleLoaded &= (*((FARPROC *) &root->api_i.d3d12_i.d3d12GetDebugInterface_i) = GetProcAddress(d3d12Module, "D3D12GetDebugInterface")) != NULL;
 			if (!moduleLoaded) {
 				FreeLibrary(d3d12Module);
 				d3d12Module = NULL;
 			}
 		}
 		if (dxgiModule != NULL && d3d12Module != NULL) {
-			root->d3d12API_i.dxgiModule_i = dxgiModule;
-			root->d3d12API_i.d3d12Module_i = d3d12Module;
+			root->api_i.d3d12_i.dxgiModule_i = dxgiModule;
+			root->api_i.d3d12_i.d3d12Module_i = d3d12Module;
 			root->supportedAPIs_r |= HbGPU_Impl_APIBits_D3D12;
 		} else {
 			if (d3d12Module != NULL) {
@@ -48,11 +52,35 @@ void HbGPU_Root_Init(HbGPU_Root * const root, HbBool validateUsage, HbMem_Tag_Ro
 		}
 		#else
 		// Statically linked.
-		root->d3d12API_i.createDXGIFactory2_i = CreateDXGIFactory2;
-		root->d3d12API_i.dxgiGetDebugInterface1_i = DXGIGetDebugInterface1;
-		root->d3d12API_i.d3d12CreateDevice_i = D3D12CreateDevice;
-		root->d3d12API_i.d3d12GetDebugInterface_i = D3D12GetDebugInterface;
+		root->api_i.d3d12_i.createDXGIFactory2_i = CreateDXGIFactory2;
+		root->api_i.d3d12_i.dxgiGetDebugInterface1_i = DXGIGetDebugInterface1;
+		root->api_i.d3d12_i.d3d12CreateDevice_i = D3D12CreateDevice;
+		root->api_i.d3d12_i.d3d12GetDebugInterface_i = D3D12GetDebugInterface;
 		root->supportedAPIs_r |= HbGPU_Impl_APIBits_D3D12;
+		#endif
+	}
+	#endif
+
+	#if HbGPU_Impl_APIs & HbGPU_Impl_APIBits_Vulkan
+	{
+		#if defined(HbPlatform_OS_Microsoft) && WINAPI_FAMILY_PARTITION(WINAPI_PARTITION_DESKTOP)
+		HMODULE const vulkanModule = LoadLibraryW(L"vulkan-1.dll");
+		if (vulkanModule != NULL) {
+			HbBool moduleLoaded = HbTrue;
+			moduleLoaded &= (*((FARPROC *) &root->api_i.vulkan_i.vkCreateInstance_i) = GetProcAddress(vulkanModule, "vkCreateInstance")) != NULL;
+			moduleLoaded &= (*((FARPROC *) &root->api_i.vulkan_i.vkGetInstanceProcAddr_i) = GetProcAddress(vulkanModule, "vkGetInstanceProcAddr")) != NULL;
+			if (moduleLoaded) {
+				root->api_i.vulkan_i.vulkanModule_i = vulkanModule;
+				root->supportedAPIs_r |= HbGPU_Impl_APIBits_Vulkan;
+			} else {
+				FreeLibrary(vulkanModule);
+			}
+		}
+		#else
+		// If not dynamically loaded, assume it's statically linked.
+		root->api_i.vulkan_i.vkCreateInstance_i = vkCreateInstance;
+		root->api_i.vulkan_i.vkGetInstanceProcAddr_i = vkGetInstanceProcAddr;
+		root->supportedAPIs_r |= HbGPU_Impl_APIBits_Vulkan;
 		#endif
 	}
 	#endif
@@ -61,7 +89,7 @@ void HbGPU_Root_Init(HbGPU_Root * const root, HbBool validateUsage, HbMem_Tag_Ro
 		#if HbGPU_Impl_APIs & HbGPU_Impl_APIBits_D3D12
 		if (root->supportedAPIs_r & HbGPU_Impl_APIBits_D3D12) {
 			ID3D12Debug * d3d12Debug;
-			if (SUCCEEDED(root->d3d12API_i.d3d12GetDebugInterface_i(&IID_ID3D12Debug, (IUnknown * *) &d3d12Debug))) {
+			if (SUCCEEDED(root->api_i.d3d12_i.d3d12GetDebugInterface_i(&IID_ID3D12Debug, (IUnknown * *) &d3d12Debug))) {
 				ID3D12Debug_EnableDebugLayer(d3d12Debug);
 				ID3D12Debug_Release(d3d12Debug);
 			}
@@ -77,7 +105,7 @@ void HbGPU_Root_Shutdown(HbGPU_Root * const root) {
 		#if HbGPU_Impl_APIs & HbGPU_Impl_APIBits_D3D12
 		if (root->supportedAPIs_r & HbGPU_Impl_APIBits_D3D12) {
 			IDXGIDebug * dxgiDebug;
-			if (SUCCEEDED(root->d3d12API_i.dxgiGetDebugInterface1_i(0, &IID_IDXGIDebug, (IUnknown * *) &dxgiDebug))) {
+			if (SUCCEEDED(root->api_i.d3d12_i.dxgiGetDebugInterface1_i(0, &IID_IDXGIDebug, (IUnknown * *) &dxgiDebug))) {
 				IDXGIDebug_ReportLiveObjects(dxgiDebug, DXGI_DEBUG_ALL, DXGI_DEBUG_RLO_ALL);
 				IDXGIDebug_Release(dxgiDebug);
 			}
@@ -85,12 +113,20 @@ void HbGPU_Root_Shutdown(HbGPU_Root * const root) {
 		#endif
 	}
 
-	#if (HbGPU_Impl_APIs & HbGPU_Impl_APIBits_D3D12) && defined(HbPlatform_OS_Microsoft) && WINAPI_FAMILY_PARTITION(WINAPI_PARTITION_DESKTOP)
-	if (root->supportedAPIs_r & HbGPU_Impl_APIBits_D3D12) {
-		HbReport_Assert_Assume(root->d3d12API_i.d3d12Module_i != NULL);
-		FreeLibrary(root->d3d12API_i.d3d12Module_i);
-		HbReport_Assert_Assume(root->d3d12API_i.dxgiModule_i != NULL);
-		FreeLibrary(root->d3d12API_i.dxgiModule_i);
+	#if defined(HbPlatform_OS_Microsoft) && WINAPI_FAMILY_PARTITION(WINAPI_PARTITION_DESKTOP)
+	#if HbGPU_Impl_APIs & HbGPU_Impl_APIBits_Vulkan
+	if (root->supportedAPIs_r & HbGPU_Impl_APIBits_Vulkan) {
+		HbReport_Assert_Assume(root->api_i.vulkan_i.vulkanModule_i != NULL);
+		FreeLibrary(root->api_i.vulkan_i.vulkanModule_i);
 	}
+	#endif
+	#if HbGPU_Impl_APIs & HbGPU_Impl_APIBits_D3D12
+	if (root->supportedAPIs_r & HbGPU_Impl_APIBits_D3D12) {
+		HbReport_Assert_Assume(root->api_i.d3d12_i.d3d12Module_i != NULL);
+		FreeLibrary(root->api_i.d3d12_i.d3d12Module_i);
+		HbReport_Assert_Assume(root->api_i.d3d12_i.dxgiModule_i != NULL);
+		FreeLibrary(root->api_i.d3d12_i.dxgiModule_i);
+	}
+	#endif
 	#endif
 }
--- HbGPU.h
+++ HbGPU.h
@@ -3,17 +3,31 @@
 #include "HbMem.h"
 
 #define HbGPU_Impl_API_D3D12 0
+#define HbGPU_Impl_API_Vulkan (HbGPU_Impl_API_D3D12 + 1)
 #define HbGPU_Impl_APIBits_D3D12 (1u << HbGPU_Impl_API_D3D12)
+#define HbGPU_Impl_APIBits_Vulkan (1u << HbGPU_Impl_API_Vulkan)
 
 #if defined(HbPlatform_OS_Microsoft)
+#include <Windows.h> // Needed for winapifamily.h and SetEventOnCompletion, and CreateSwapChainForHwnd on the desktop partition.
+#if WINAPI_FAMILY_PARTITION(WINAPI_PARTITION_DESKTOP)
+#define HbGPU_Impl_APIs (HbGPU_Impl_APIBits_D3D12 | HbGPU_Impl_APIBits_Vulkan)
+#else
 #define HbGPU_Impl_APIs HbGPU_Impl_APIBits_D3D12
-#include <Windows.h>
+#endif
 #endif
 #if HbGPU_Impl_APIs & HbGPU_Impl_APIBits_D3D12
 #include <d3d12.h>
 #include <d3d12sdklayers.h>
 #include <dxgi1_4.h>
 #include <dxgidebug.h>
+#endif
+#if HbGPU_Impl_APIs & HbGPU_Impl_APIBits_D3D12
+#if defined(HbPlatform_OS_Microsoft) && WINAPI_FAMILY_PARTITION(WINAPI_PARTITION_DESKTOP)
+#ifndef VK_NO_PROTOTYPES
+#define VK_NO_PROTOTYPES
+#endif
+#endif
+#include "Vulkan-Headers/include/vulkan/vulkan.h"
 #endif
 
 #ifndef HbGPU_Impl_APIs
@@ -27,23 +41,40 @@ extern "C" {
 typedef unsigned HbGPU_Impl_API;
 typedef unsigned HbGPU_Impl_APIBits;
 
+/*******************
+ * System API setup
+ *******************/
+
 typedef struct HbGPU_Root {
 	HbMem_Tag_Root * memTagRoot_e;
 	HbBool validateUsage_r;
 	HbGPU_Impl_APIBits supportedAPIs_r;
-	#if HbGPU_Impl_APIs & HbGPU_Impl_APIBits_D3D12
 	struct {
-		#if defined(HbPlatform_OS_Microsoft) && WINAPI_FAMILY_PARTITION(WINAPI_PARTITION_DESKTOP)
-		HMODULE dxgiModule_i;
-		HMODULE d3d12Module_i;
-		HMODULE d3d12SDKLayersModule_i;
+		#if HbGPU_Impl_APIs & HbGPU_Impl_APIBits_D3D12
+		struct {
+			#if defined(HbPlatform_OS_Microsoft) && WINAPI_FAMILY_PARTITION(WINAPI_PARTITION_DESKTOP)
+			HMODULE dxgiModule_i;
+			HMODULE d3d12Module_i;
+			#endif
+			HRESULT (WINAPI * createDXGIFactory2_i)(UINT flags, REFIID riid, void * * factory);
+			HRESULT (WINAPI * dxgiGetDebugInterface1_i)(UINT flags, REFIID riid, void * * debug);
+			PFN_D3D12_CREATE_DEVICE d3d12CreateDevice_i;
+			PFN_D3D12_GET_DEBUG_INTERFACE d3d12GetDebugInterface_i;
+		} d3d12_i;
 		#endif
-		HRESULT (WINAPI * createDXGIFactory2_i)(UINT flags, REFIID riid, void * * factory);
-		HRESULT (WINAPI * dxgiGetDebugInterface1_i)(UINT flags, REFIID riid, void * * debug);
-		PFN_D3D12_CREATE_DEVICE d3d12CreateDevice_i;
-		PFN_D3D12_GET_DEBUG_INTERFACE d3d12GetDebugInterface_i;
-	} d3d12API_i;
-	#endif
+		#if HbGPU_Impl_APIs & HbGPU_Impl_APIBits_Vulkan
+		struct {
+			#if defined(HbPlatform_OS_Microsoft) && WINAPI_FAMILY_PARTITION(WINAPI_PARTITION_DESKTOP)
+			HMODULE vulkanModule_i;
+			#endif
+			PFN_vkCreateInstance vkCreateInstance_i;
+			PFN_vkGetInstanceProcAddr vkGetInstanceProcAddr_i;
+		} vulkan_i;
+		#endif
+		#if HbGPU_Impl_APIs == 0
+		HbByte unimplemented_i;
+		#endif
+	} api_i;
 } HbGPU_Root;
 
 void HbGPU_Root_Init(HbGPU_Root * const root, HbBool validateUsage, HbMem_Tag_Root * const memTagRoot);
